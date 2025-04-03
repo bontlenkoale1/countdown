75fc3e19480d6cd71e28759bf4e5c09764f9b959
bontlenkoale1 <bontlenkoale20@gmail.com>
2025-04-04 00:39:02 +0200

    updated custom message and its styling

--- script.js
+++ script.js
@@ -90,27 +90,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
-/*function startConfetti() {
-      const duration = 30 * 1000;
-      const animationEnd = Date.now() + duration;
-      const defaults = {
-        startVelocity: 30,
-        spread: 360,
-        ticks: 60,
-        zIndex: 0,
-        particleCount: 200,
-        origin: { y: 0.6 },
-      };
-      const interval = setInterval(function () {
-        const timeLeft = animationEnd - Date.now();
-        if (timeLeft <= 0) {
-          return clearInterval(interval);
-        }
-        const particleCount = 50 * (timeLeft / duration);
-        confetti({
-          ...defaults,
-          particleCount,
-          origin: { x: Math.random() * 0.8 + 0.1, y: Math.random() - 0.2 }
-        });
-      }, 250);
-    }*/
